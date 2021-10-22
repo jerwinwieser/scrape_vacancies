4bf369089f53322317d96a493d0b8fc50a4871d9
jerwinwieser <j2.wieser@gmail.com>
2021-10-22 20:32:32 +0200

    upt

---
+++ scrape_jobs.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(rvest)
+library(stringr)
+
+url <- "https://www.randstad.nl/vacatures?pagina=1"
+
+n_jobs <- url %>%
+  read_html() %>%
+  html_nodes(xpath = '//*[@class="jobboardbar__resultcounter"]') %>%
+  html_text() %>%
+  str_extract(pattern = "(?<=\\().*(?=\\))") %>%
+  str_sub(-5,-1) %>%
+  str_replace("\\.", "") %>%
+  strtoi()
+
+n_pages <- ceiling(n_jobs / 15)
+
+read_jobs <- function(page) {
+  url <- paste0("https://www.randstad.nl/vacatures?pagina=", as.character(page))
+  url %>%
+    read_html() %>%
+    html_nodes(xpath = '//*[@class="vacancy-tile"]') %>%
+    html_text2() %>%
+    as_tibble()
+}
+
+jobs <- seq(1,n_pages) %>%
+  map_dfr(read_jobs, .id = "page") %>%
+  mutate(job_number = row_number(), .before = 1) %>%
+  filter(str_detect(tolower(value), "data")) %>%
+  separate(col = value, into = c("title", "city", "salaris"), sep = "\n")
+
