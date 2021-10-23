9630559fea120f5f476fdc9857d6fb9b7b06dfd5
jerwinwieser <j2.wieser@gmail.com>
2021-10-23 21:57:30 +0200

    scrape jobs indeed

--- scrape_jobs_indeed.R
+++ scrape_jobs_indeed.R
@@ -3,3 +3,51 @@ library(purrr)
 library(rvest)
 library(tidyr)
 library(stringr)
+
+dtime_start <- paste("scraping started : ", Sys.time())
+
+scrape_jobs_indeed <- function(job_number) {
+  
+  job_number <- 1
+  
+  url <- paste0("https://nl.indeed.com/vacatures?q=data&start=", as.character(job_number))
+  print(paste("scraping :", url))
+  
+  html <- url %>% 
+    read_html()
+  
+  df_jobs <- html %>% 
+    html_nodes('h2') %>% 
+    html_text2() %>% 
+    as_tibble() %>% 
+    mutate(title = str_replace(value, "nieuw\n", ""), .keep = "none")
+  
+  df_date <- html %>% 
+    html_nodes(xpath = '//*[@class="date"]') %>% 
+    html_text2() %>% 
+    as_tibble() %>% 
+    rename(date = value)
+  
+  df_out <- bind_cols(df_jobs, df_date)
+  
+  return(df_out)
+  
+}
+
+job_numbers <- seq(1,25)*10
+
+jobs <- job_numbers %>% 
+  map_dfr(scrape_jobs_indeed, .id = "job_page_start") %>% 
+  mutate(days_ago = strtoi(str_extract_all(date, "(\\d)+")),
+         days_ago = if_else(is.na(days_ago), 0L, days_ago),
+         days_ago_str = paste(days_ago, "days ago")) %>% 
+  arrange(days_ago) %>% 
+  select(-c(date, job_page_start, days_ago))
+
+dtime_finish <- paste("scraping finished: ", Sys.time())
+
+output_file <- "~/jobs/jobs_indeed.txt"
+
+write.table(jobs, output_file, append = FALSE, sep = " ", dec = ".", row.names = FALSE, col.names = FALSE)
+write.table(dtime_start, output_file, append = TRUE, sep = " ", dec = ".", row.names = FALSE, col.names = FALSE)
+write.table(dtime_finish, output_file, append = TRUE, sep = " ", dec = ".", row.names = FALSE, col.names = FALSE)
