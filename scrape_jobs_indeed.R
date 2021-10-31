db4a276a66b439b058825e187caef5297279f34c
jerwinwieser <j2.wieser@gmail.com>
2021-10-31 18:11:44 +0100

    upt

--- scrape_jobs_indeed.R
+++ scrape_jobs_indeed.R
@@ -8,7 +8,7 @@ dtime_start <- paste("scraping started : ", Sys.time())
 
 scrape_jobs_indeed <- function(job_number) {
   
-  job_number <- 1
+  # job_number <- 1
   
   url <- paste0("https://nl.indeed.com/vacatures?q=data&start=", as.character(job_number))
   print(paste("scraping :", url))
@@ -51,7 +51,7 @@ jobs <- job_numbers %>%
          days_ago_str = paste(days_ago, "days ago")) %>% 
   arrange(days_ago) %>% 
   filter(days_ago < 2) %>% 
-  distinct(title, days_ago_str)
+  distinct(title, days_ago_str, value)
 
 dtime_finish <- paste("scraping finished: ", Sys.time())
 
